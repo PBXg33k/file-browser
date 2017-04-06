6bd7fd86bc6bfbf57749143b613f8a4feea6a441
Oguzhan Uysal <development@oguzhanuysal.eu>
2017-04-06 16:59:40 +0200

    Allow access to app_dev from any local machine (network)

--- web/app_dev.php
+++ web/app_dev.php
@@ -8,11 +8,19 @@ use Symfony\Component\Debug\Debug;
 // for more information
 //umask(0000);
 
+function is_local_address($ip) {
+    return !filter_var(
+            $ip,
+                FILTER_VALIDATE_IP,
+                FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE
+        );
+}
+
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')) || php_sapi_name() === 'cli-server')
+    || !(is_local_address($_SERVER['REMOTE_ADDR']) || php_sapi_name() === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
